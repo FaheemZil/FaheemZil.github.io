b5b585528575c42e8368208e1f850ed65656ebcd
FaheemZil <147126414+FaheemZil@users.noreply.github.com>
2024-05-09 18:55:12 +0530

    refreshing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,7 +55,6 @@ const RESOURCES = {"assets/AssetManifest.bin": "ad72655352beff4c94fd1b3ac18aff6d
 "assets/assets/images/freshbooks.svg": "e1149d1396433bd3ec32cec34c7072ab",
 "assets/assets/images/google.svg": "88c5bef508b9b26ccbcf922e73fd7510",
 "assets/assets/images/green_bidirectional.svg": "16d4de8c1bfc01c47cb3596eb7f3881c",
-"assets/assets/images/group_check.svg": "46a30e6839b7726846b2404c5826b367",
 "assets/assets/images/help.svg": "e18432ad9a78caa8c516fced36a6ed0f",
 "assets/assets/images/hourglass_disabled.svg": "7bee1900aef22eabcfa3619ad74690de",
 "assets/assets/images/info.svg": "379022006423b3c8a59ab57db8713794",
@@ -449,9 +448,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "ad72655352beff4c94fd1b3ac18aff6d
 "icons/Icon-512.png": "b3362faa3094aca9b8edff0e04e20cb2",
 "icons/Icon-maskable-192.png": "46a6e3b343c24abf7debc3083afca6c9",
 "icons/Icon-maskable-512.png": "b3362faa3094aca9b8edff0e04e20cb2",
-"index.html": "59dafd8cce4bccdd60f014eeca7e90d6",
-"/": "59dafd8cce4bccdd60f014eeca7e90d6",
-"main.dart.js": "44d84ec45285293bfcaaaec540cf0c03",
+"index.html": "95195bbbfe61932b6714606732241266",
+"/": "95195bbbfe61932b6714606732241266",
+"main.dart.js": "a4a02f8ac93a04dc6fcdc60ebc1e5e5f",
 "manifest.json": "d3e066ceb4129054a98a29c990cc0b99",
 "version.json": "9a89916279f7ee02f882525ec654e575"};
 // The application shell files that are downloaded before a service worker can
